5941e57d6771532e12a8a0e12918d088aa345983
flyiq44032014-cyber <fly.iq4403.2014@gmail.com>
2026-03-24 16:00:32 +0300

    Remove node_modules .idea dist + update .gitignore

--- dist/rsc/controllers/photoController.d.ts
+++
@@ -1,4 +0,0 @@
-import * as express from "express";
-export declare const getPhotos: (req: express.Request, res: express.Response) => void;
-export declare const createPhotoHandler: (req: express.Request, res: express.Response) => express.Response<any, Record<string, any>> | undefined;
-//# sourceMappingURL=photoController.d.ts.map
--- dist/rsc/routes/photos.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import * as photoController from "../controllers/photoController.js";
-const router = express.Router();
-router.get("/", photoController.getPhotos);
-router.post("/", photoController.createPhotoHandler);
-export default router;
-//# sourceMappingURL=photos.js.map
